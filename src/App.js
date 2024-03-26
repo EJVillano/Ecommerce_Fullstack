fb4e93d580b43c9e0d42d3c8baa135f045edec1b
edgarjohnvillano <edgarjohnvillano@gmail.com>
2024-03-26 10:35:42 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import { useState, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+
+//Route Components
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+
+import AppNavbar from './components/AppNavbar';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Logout from './pages/Logout';
+import Error from './pages/Error';
+
+import './App.css';
+import { UserProvider } from './UserContext';
+
+
+function App() {
+
+ 
+  const [user, setUser] = useState({
+
+    id: null,
+    isAdmin: null 
+
+  })
+
+  
+  const unsetUser = () =>{
+    localStorage.clear()
+  }
+
+  
+  useEffect(()=>{
+   
+    fetch(`${process.env.REACT_APP_API_URL}/users`,{
+      headers:{
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    })
+    .then(res=>res.json())
+    .then(data=>{
+
+      console.log(data)
+
+      if(typeof data.user !== "undefined"){
+        setUser({
+          id: data.user._id,
+          isAdmin: data.user.isAdmin
+        })
+      }else{
+        setUser({
+          id: null,
+          isAdmin: null
+        })
+      }
+
+    })
+  },[])
+
+
+  return (
+    <UserProvider value={{user, setUser, unsetUser}}>
+    
+        <Router>
+         
+            < AppNavbar />
+            <Container fluid>
+                  <Routes>
+                  
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/register" element={<Register/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/logout" element={<Logout/>}/>
+                    <Route path="*" element={<Error />} />
+                  </Routes>
+            </Container>
+        </Router>
+      </UserProvider>
+    );
+
+}
+
+
+export default App;
